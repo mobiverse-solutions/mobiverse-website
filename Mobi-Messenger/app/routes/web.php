caf734cb154aa0de26309eb01661ce5a86889641
Nana Gyasiwah <nanagyasiwah49@gmail.com>
2025-12-07 22:04:12 -1200

    mobiverse website

--- Mobi-Messenger/app/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContactFormController;
-
-Route::get('/', function () {
-    return view('home');
-});
-Route::post('/contactform', ContactFormController::class);
--- uzo-website/test.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "Current year: " . date('Y');
-?>
